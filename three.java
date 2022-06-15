d0c811db5eaa0b194cdc4a9319c73401d693eebb
kishore.mightymech <kishore.mightymech@gmail.com>
2022-06-15 13:50:29 +0530

    a

---
+++ three.java
@@ -0,0 +1,8 @@
+Docker:
+$ sudo su -               (to change to root user)
+
+
+This script is meant for quick & easy install via:
+ # curl -fsSL https://get.docker.com -o get-docker.sh
+ # sh get-docker.sh
+ # docker --version
